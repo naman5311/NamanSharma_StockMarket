46f746a9b318c357ce93bc18d7bc7083be5f6db7
Naman <naman.5311@gmail.com>
2020-08-03 03:21:29 +0530

    chart added

--- UI/StockMarket/src/app/Containers/compare-company-view/compare-company-view.component.ts
+++ UI/StockMarket/src/app/Containers/compare-company-view/compare-company-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
+import { Chart } from 'chart.js';  
+import { HttpClient } from '@angular/common/http';  
+import { Stockprice } from 'src/app/Models/stockprice';
 
 @Component({
   selector: 'app-compare-company-view',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareCompanyViewComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  url = 'http://localhost:5003/api/StockPrice/GetStockPrices/';  
+  data: Stockprice[];  
+  Player = [];  
+  Run = [];  
+  Linechart = [];  
+  constructor(private httpClient: HttpClient) { }  
+  ngOnInit() {  
+    this.httpClient.get(this.url+"Yes Bank").subscribe((result: Stockprice[]) => {  
+      result.forEach(x => {  
+        this.Player.push(x.date);  
+        this.Run.push(x.currentPrice);  
+      });  
+      this  
+      this.Linechart = new Chart('canvas', {  
+        type: 'line',  
+        data: {  
+          labels: this.Player,  
+          datasets: [  
+            {  
+              data: this.Run,  
+              borderColor: '#3cb371',  
+              backgroundColor: "#0000FF",  
+            }  
+          ]  
+        },  
+        options: {  
+          legend: {  
+            display: false  
+          },  
+          scales: {  
+            xAxes: [{  
+              display: true  
+            }],  
+            yAxes: [{  
+              display: true  
+            }],  
+          }  
+        }  
+      });  
+    });  
+  }  
 }
---
+++ UI/StockMarket/src/app/Models/stockprice.ts
@@ -0,0 +1,10 @@
+import { Time } from '@angular/common'
+
+export class Stockprice {
+    stockId:number
+    CompanyCode:string
+    StockExchange:string
+    CurrentPrice:number
+    Date:Date
+    Time:Time
+}
